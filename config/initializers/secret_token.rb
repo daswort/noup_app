f7802c77faddd45aed4f05400c6d92b4a58438aa
daswort <firstcommand@gmail.com>
2012-08-12 12:33:27 -0400

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noup::Application.config.secret_token = 'e683dffd0b2bb0576ace760fbb895ba6ac93371409453b90620470c74f51043c74c65cc747ef71a42c3b842b66b8505d54fda6e6df89fbb8b73df644db2516e8'
